0931423ab54860be6fb597c405d9ca1b2beca409
Ricardo T. Ono <ric_tadashi@hotmail.com>
2016-08-18 22:22:08 -0300

    Add files via upload

---
+++ Aula 08182016/exer1.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Aula 2°</title>hu
+    </head>
+<body>
+    <p>Hello World</p>
+    <?php
+        echo'<p><b>MAXIMOOOOOW</b></p>';
+//        echo phpinfo();
+//        echo phpversion();
+        $x = 1;
+        $s = 2;
+        echo 'a SOMA é: '.($x + $s);
+        echo '<br>a SOMA é: $s'; //não faz 
+        echo "<br>a SOMA é: $s"; //aspas duplas faz a conversão 
+    ?>
+</body>
+</html>
